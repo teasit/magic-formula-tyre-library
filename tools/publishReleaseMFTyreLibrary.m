6719ab591423bd0b6d5b9847a8a865dd6b743c84
Tom Teasdale <teasi023@gmail.com>
2022-05-01 16:03:21 +0200

    release automation ready

--- tools/publishReleaseMFTyreLibrary.m
+++ tools/publishReleaseMFTyreLibrary.m
@@ -1,29 +1,40 @@
-function [outputArg1,outputArg2] = publishReleaseMFTyreLibrary(inputArg1,inputArg2)
-%PUBLISHRELEASEMFTYRELIBRARY Summary of this function goes here
-
-Interactively create a release
-gh release create
-
-Interactively create a release from specific tag
-$ gh release create v1.2.3
-
-Non-interactively create a release
-$ gh release create v1.2.3 --notes "bugfix release"
-
-Use automatically generated release notes
-$ gh release create v1.2.3 --generate-notes
-
-Use release notes from a file
-$ gh release create v1.2.3 -F changelog.md
-
-Upload all tarballs in a directory as release assets
-$ gh release create v1.2.3 ./dist/*.tgz
-
-Upload a release asset with a display label
-$ gh release create v1.2.3 '/path/to/asset.zip#My display label'
-
-Create a release and start a discussion
-$ gh release create v1.2.3 --discussion-category "General"
-
+function publishReleaseMFTyreLibrary(version, title, changelogFile, packageFile, packagerFile)
+%PUBLISHRELEASEMFTYRELIBRARY Automatic release to GitHub
+arguments
+    version char
+    title char = char.empty
+    changelogFile char {mustBeFile} = 'CHANGELOG.md'
+    packageFile char = 'MFTyreLibrary.mltbx'
+    packagerFile char = 'MFTyreLibraryToolboxPackager.prj'
+end
+pattern = 'v' + digitsPattern() + '.' + digitsPattern() + '.' + digitsPattern();
+versionInvalid = ~matches(version, pattern);
+if versionInvalid
+    error('Invalid version pattern. Example: v1.0.1')
 end
 
+matlab.addons.toolbox.toolboxVersion(packagerFile, erase(version, 'v'));
+matlab.addons.toolbox.packageToolbox(packagerFile);
+
+if isempty(title)
+    title = version;
+end
+
+cmd = "gh release create %s --title %s --notes-file %s";
+cmd = sprintf(cmd, version, title, changelogFile);
+[status, cmdout] = system(cmd);
+if status == 1
+    warning('Failed to create release. Command-Line output:')
+    disp(cmdout)
+    return
+end
+
+cmd = "gh release upload %s %s";
+cmd = sprintf(cmd, version, packageFile);
+[status, cmdout] = system(cmd);
+if status == 1
+    warning('Failed to upload packaged toolbox. Command-Line output:')
+    disp(cmdout)
+    return
+end
+end
